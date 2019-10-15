70e78345797c013411bdd53273006d43497296c8
Murat Bozkurt <muratbozkurt84@gmail.com>
2019-10-15 22:13:12 +0300

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { FooterComponent } from './pages/_layout/footer/footer.component';
+import { HeaderComponent } from '../_layout/header/header.component';
+import { SidebarComponent } from '../_layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
